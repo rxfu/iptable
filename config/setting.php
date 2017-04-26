27e47c643879c08cfd6971c04bfe0d3802d7781c
Stephen Fu <stephen.frx@gmail.com>
2017-04-26 11:16:08 +0800

    add system setting params

---
+++ config/setting.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'author'      => 'Fu Rongxin',
+	'keywords'    => 'IP management,Laravel,Vue.js,Material Design',
+	'description' => 'IP management based on Vue.js and Laravel',
+	'name'        => '广西师范大学图书馆IP管理系统',
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="{{ config('setting.author') }}">
+    <meta name="keywords" content="{{ config('setting.keywords') }}">
+    <meta name="description" content="{{ config('setting.description') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('setting.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
