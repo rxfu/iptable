5119c19893f71bdeb2cd3d879973094e75fb16b0
Stephen Fu <stephen.frx@gmail.com>
2017-05-02 17:59:10 +0800

    update compose menu method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,41 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+
+		View::composer('*', function ($view) {
+
+			$routes = [
+				[
+					'href' => route('login'),
+					'name' => '登录',
+				],
+				[
+					'href' => route('register'),
+					'name' => '注册',
+				],
+			];
+
+			$view->with('menu', json_encode($routes));
+		});
+	}
 }
